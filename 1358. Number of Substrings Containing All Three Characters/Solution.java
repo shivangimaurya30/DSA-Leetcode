c84305c7c9cc5150bcd33219711dc7e930b4c1e0
Shivangi Maurya <118928647+shivangimaurya30@users.noreply.github.com>
2025-04-30 18:16:21 +0530

    Create Solution.java

---
+++ 1358. Number of Substrings Containing All Three Characters/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int count[]={0,0,0};
+        int res=0;
+        int i=0; 
+        for (int j=0; j<s.length();j++) {
+            count[s.charAt(j)-'a']++; 
+            while (count[0]>0&& count[1]>0 &&count[2]>0) {
+                res+=s.length()-j; 
+                count[s.charAt(i)-'a']--; 
+                i++;
+            }
+        }
+        return res;
+    }
+}
